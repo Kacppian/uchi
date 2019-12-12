5100e7d25d54d3ca9118f16f5ece03be122a5b35
Kaushik ASP <aspkaushik@gmail.com>
2019-12-12 08:34:05 +0530

    testing

--- cmd/go/ingest_data.go
+++ cmd/go/ingest_data.go
@@ -56,6 +56,8 @@ type Property struct {
 // Properties is a list of property instances
 type Properties []Property
 
+var db *sql.DB
+
 func unmarshallJSON(jsonFilename string) Properties {
 	var properties Properties
 	data, err := ioutil.ReadFile(jsonFilename)
@@ -64,6 +66,49 @@ func unmarshallJSON(jsonFilename string) Properties {
 	}
 	json.Unmarshal([]byte(data), &properties)
 	return properties
+}
+
+func insertIntoDb(r Property, i int) {
+	integerRent := 0
+	integerDeposit := 0
+
+	fmt.Println("Processing ", i)
+
+	if r.Rent != "" {
+		integerRent, _ = strconv.Atoi(r.Rent)
+
+		// if err != nil {
+		// 	fmt.Println(r.Rent)
+		// }
+	}
+
+	if r.Deposit != "" {
+		integerDeposit, _ = strconv.Atoi(r.Deposit)
+
+		// if err != nil {
+		// 	fmt.Println(r.Deposit)
+		// }
+	}
+
+	sqlStatement := `
+	INSERT INTO properties (title, lat, lng, rent, deposit, link_to, furnishing, age, preferred_tenants, move_in_date, images)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
+
+	trimmedTitle := strings.TrimSpace(r.Title)
+	trimmedFurnishing := strings.TrimSpace(r.AdditionalAttributes.Furnishing)
+	trimmedAge := strings.TrimSpace(r.AdditionalAttributes.Age)
+	trimmedTenantPreference := strings.TrimSpace(r.AdditionalAttributes.PreferredTenants)
+	trimmedMoveInDate := strings.TrimSpace(r.AdditionalAttributes.MoveInDate)
+
+	_, err := db.Exec(sqlStatement, trimmedTitle, r.Coordinates.Lat, r.Coordinates.Long,
+		integerRent, integerDeposit, r.LinkTo, trimmedFurnishing, trimmedAge,
+		trimmedTenantPreference, trimmedMoveInDate, pq.Array(r.Images))
+
+	if err != nil {
+		fmt.Println("there's an error")
+		fmt.Println(err)
+		fmt.Println(i)
+	}
 }
 
 func main() {
@@ -86,48 +131,9 @@ func main() {
 
 	fmt.Println("successfully connected")
 
-	unmarshalledProperties := unmarshallJSON("properties.json")
+	unmarshalledProperties := unmarshallJSON("../properties.json")
 
 	for i, r := range unmarshalledProperties {
-		integerRent := 0
-		integerDeposit := 0
-
-		fmt.Println("Processing ", i)
-
-		if r.Rent != "" {
-			integerRent, err = strconv.Atoi(r.Rent)
-
-			if err != nil {
-				fmt.Println(r.Rent)
-			}
-		}
-
-		if r.Deposit != "" {
-			integerDeposit, err = strconv.Atoi(r.Deposit)
-
-			if err != nil {
-				fmt.Println(r.Deposit)
-			}
-		}
-
-		sqlStatement := `
-		INSERT INTO properties (title, lat, lng, rent, deposit, link_to, furnishing, age, preferred_tenants, move_in_date, images)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
-
-		trimmedTitle := strings.TrimSpace(r.Title)
-		trimmedFurnishing := strings.TrimSpace(r.AdditionalAttributes.Furnishing)
-		trimmedAge := strings.TrimSpace(r.AdditionalAttributes.Age)
-		trimmedTenantPreference := strings.TrimSpace(r.AdditionalAttributes.PreferredTenants)
-		trimmedMoveInDate := strings.TrimSpace(r.AdditionalAttributes.MoveInDate)
-
-		_, err = db.Exec(sqlStatement, trimmedTitle, r.Coordinates.Lat, r.Coordinates.Long,
-			integerRent, integerDeposit, r.LinkTo, trimmedFurnishing, trimmedAge,
-			trimmedTenantPreference, trimmedMoveInDate, pq.Array(r.Images))
-
-		if err != nil {
-			fmt.Println("there's an error")
-			fmt.Println(err)
-			fmt.Println(i)
-		}
+		go insertIntoDb(r, i)
 	}
 }
